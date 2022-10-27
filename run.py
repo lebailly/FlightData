d4993c71e1b7d92635e3198d527432a5a28275f1
Chris LeBailly <chris.lebailly@gmail.com>
2022-10-27 15:46:54 -0500

    Run script runs all classes

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 import yaml
-from fore_flight import ForeFlightUserWaypoints
+from fore_flight import *
 
 with open('credentials.yml') as fp:
-    creds = yaml.safe_load(fp)
+    creds = yaml.safe_load(fp)['ForeFlight']
 
-fore_flight_logs = ForeFlightUserWaypoints(**creds)
-fore_flight_logs.run()
-#fore_flight.download_tracklog_data(download_kml=True) # took only 4:35 to run 
+fore_flight_waypoints = ForeFlightUserWaypoints(**creds)
+fore_flight_waypoints.run()
+
+fore_flight_tracklogs = ForeFlightTrackLogs(**creds)
+fore_flight_tracklogs.run(download_kml=True)
+
+fore_flight_log_pics = ForeFlightLogs(**creds)
+fore_flight_log_pics.run()
